aac33d27e80a8fd41f0b0eeba3afa762fb43fa09
alanwangmodify <578090380@qq.com>
2015-07-14 14:40:24 +0800

    Initial Commit

---
+++ ButtonCirlcleDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ButtonCirlcleDemo
+//
+//  Created by choumei on 15/7/14.
+//  Copyright (c) 2015年 WangXiaoShu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ButtonCirlcleDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ButtonCirlcleDemo
+//
+//  Created by choumei on 15/7/14.
+//  Copyright (c) 2015年 WangXiaoShu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
